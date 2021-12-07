43fd4db96791f461e9b5dfda8b64fd76b21d6823
Carol_Inglethe <carolinainglethe@gmail.com>
2021-12-07 17:19:59 -0300

    Requesito 2 - encontrar talker apartir do id no parametro do rota

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const fs = require('fs/promises');
 
 const readFileTalkers = require('./utils/readFileTalkers.js');
 
@@ -21,6 +20,18 @@ app.get('/', (_request, response) => {
 app.get('/talker', async (_req, res) => {
   const talkers = await readFileTalkers();
   return res.status(200).json(talkers);
+});
+
+// Requesito 2:
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkers = await readFileTalkers();
+
+  const userId = talkers.find((talker) => talker.id === Number(id));
+
+  if (!userId) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+
+  return res.status(200).json(userId);
 });
 
 // FIM REQUESITOS.
