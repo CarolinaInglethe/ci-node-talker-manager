97583a76a7a6f135b9fec7319c6a24f29db7d99a
Carol_Inglethe <carolinainglethe@gmail.com>
2021-12-09 15:09:13 -0300

    refatora requesito 3

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.get('/talker/:id', async (req, res) => {
 
 // Requesito 3:
 app.post('/login', validateEmail, validatePassword, (req, res) => {
-  const randomString = () => Math.random().toString(16).substr(2);
+  const randomString = () => Math.random().toString(36).substr(2);
   const generateToken = () => (randomString() + randomString()).substr(0, 16);
   // string.substr(start, length)
   // referencia : https://medium.com/@norbertofariasmedeiros/five-steps-como-gerar-um-random-token-em-javascript-1e1488a15d28
