fae9a0530fc94b2c3591331bcf25db9def19ff0a
Carol_Inglethe <carolinainglethe@gmail.com>
2021-12-10 20:05:22 -0300

    Refatora requesito 4 - validacaodowatchedAt com Regex

--- middlewares/validateTalkWatchedAt.js
+++ middlewares/validateTalkWatchedAt.js
@@ -1,5 +1,6 @@
 const validateTalkWatchedAt = (req, res, next) => {
     const { talk } = req.body;
+    const regexData = /^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/;
     
     if (!talk || !talk.watchedAt) {
         return res.status(400)
@@ -7,14 +8,14 @@ const validateTalkWatchedAt = (req, res, next) => {
             message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios', 
         });
     }
-    if (!(talk.watchedAt instanceof Date)) {
+    if (!regexData.test(talk.watchedAt)) {
         return res.status(400)
         .json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
     }
     next();
 };
 
-// https://www.horadecodar.com.br/2021/04/29/como-detectar-uma-data-invalida-em-javascript/
-// O operador instanceof testa se um objeto tem, em seu prototype, a função construtora.
+// https://www.guj.com.br/t/resolvido-como-validar-data-com-java-script/276656/2
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Guide/Regular_Expressions
 
 module.exports = validateTalkWatchedAt;
