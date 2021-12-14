d407befb976cfa45c0d25ec5ee8d110510522fb2
Carol_Inglethe <carolinainglethe@gmail.com>
2021-12-13 22:02:06 -0300

    corrige rota para arquivo talker.json

--- utils/readFileTalkers.js
+++ utils/readFileTalkers.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 const readFileTalkersFunction = () => {
     try {
-      const talkers = fs.readFileSync('../talker.json');
+      const talkers = fs.readFileSync('./talker.json');
       const parseTalkers = JSON.parse(talkers);
 
       return parseTalkers;
--- utils/writeFileTalkers.js
+++ utils/writeFileTalkers.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 const writeFileTalkers = (NewTalkers) => {
-    fs.writeFileSync('../talker.json', JSON.stringify(NewTalkers));
+    fs.writeFileSync('./talker.json', JSON.stringify(NewTalkers));
 };
 
 module.exports = writeFileTalkers;
