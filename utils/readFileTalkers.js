1f7574b50dfc075a6a7544fdcd18f06591ba3ffc
Carol_Inglethe <carolinainglethe@gmail.com>
2021-12-13 21:58:55 -0300

    refatora req 4 - corrige caminho para pasta talkers no readfile

--- utils/readFileTalkers.js
+++ utils/readFileTalkers.js
@@ -2,11 +2,11 @@ const fs = require('fs');
 
 const readFileTalkersFunction = () => {
     try {
-      const talkers = fs.readFileSync('./talker.json');
+      const talkers = fs.readFileSync('../talker.json');
       const parseTalkers = JSON.parse(talkers);
 
       return parseTalkers;
-    } catch (err) {
+    } catch (error) {
       return [];
     }
   };
