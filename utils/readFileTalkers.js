05bd530aa47c9411327796d1822cc31aab25f4ee
Carol_Inglethe <carolinainglethe@gmail.com>
2021-12-18 12:59:48 -0300

    req 5

--- utils/readFileTalkers.js
+++ utils/readFileTalkers.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 const readFileTalkersFunction = () => {
     try {
-      const talkers = fs.readFileSync('./talker.json');
+      const talkers = fs.readFileSync('./talker.json', 'utf-8');
       const parseTalkers = JSON.parse(talkers);
 
       return parseTalkers;
